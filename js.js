79f3b2ae26452248e951777ab991048abd820a3a
yellowside <30918006+yellowside@users.noreply.github.com>
2020-08-20 08:10:52 +0800

    Create js.js

---
+++ js.js
@@ -0,0 +1,8 @@
+//tongji
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?0388e27ad00b627fe43bd8d8dea61a0b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
