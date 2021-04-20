4baea6fd01b2db68a2775788b469bf24a35b8925
yellowside <30918006+yellowside@users.noreply.github.com>
2021-04-20 16:13:12 +0800

    Update jiexi.user.js

--- jiexi.user.js
+++ jiexi.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         【冰豆云播】免费在线观看VIP视频，支持爱奇艺，优酷，乐视tv，腾讯视频，土豆，芒果TV，搜狐视频，Acfun，bilibili，PPTV，华数TV等主流影视平台
-// @namespace    https://vod.bingdou.net
+// @namespace    https://vod.bingdou.xyz
 // @version      0.3
 // @description  只专注于一个功能，简洁干净，在视频播放页左侧点击VIP按钮，跳转到新页面即可免费在线观看vip视频
 // @author       simples
@@ -38,6 +38,6 @@
     var vipBtn = "<div id='vipBtn' style='cursor:pointer;z-index:998;position:fixed;left:10px;top:300px;'><img src='https://cdn.jsdelivr.net/gh/yellowside/jquery/vip.gif' height='55' ></div>";
     $("body").append(vipBtn);
     $('#vipBtn').click(function() {
-        window.location.href = "https://vod.bingdou.net/?url=" + encodeURIComponent(window.location.href);
+        window.location.href = "https://vod.bingdou.xyz/?url=" + encodeURIComponent(window.location.href);
     });
 })();
