7e4ddca0a22435def0defff665305f74e6b95e83
yellowside <30918006+yellowside@users.noreply.github.com>
2021-07-10 15:21:51 +0800

    Update a.js

--- a.js
+++ a.js
@@ -5,4 +5,4 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-document.write('<scr'+'ipt async defer src="https://d.ytop8.com/z.aspx?action=c&z1=7&z2=7289&z3=&z4=2&z5=AdCode_zcgsm&z6=60x60&z7=1&z8=1&z9=&z10=&z50=641249" id="641249"></scr'+'ipt>');
+document.write('<scr'+'ipt async defer src="https://d58.d58gg.com/z.aspx?action=c&z1=7&z2=7289&z3=&z4=2&z5=AdCode_zcgsm&z6=60x60&z7=1&z8=1&z9=&z10=&z50=832836" id="832836"></scr'+'ipt>');
