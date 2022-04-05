3ce10cd8e8081681b98e5a32db0a6b8fbefb121c
yellowside <30918006+yellowside@users.noreply.github.com>
2022-04-05 18:07:54 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,9 +1,8 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?0388e27ad00b627fe43bd8d8dea61a0b";
+  hm.src = "https://hm.baidu.com/hm.js?0388e27ad00b627fe43bd8d8dea61a0b";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-document.write('<scr'+'ipt src="https://api.zihengfx.com/w.aspx?action=a&w32=712&w2=7603&w3="></scr'+'ipt>');
 document.write('<scr'+'ipt src="https://api.zihengfx.com/w.aspx?action=a&w32=709&w2=7603&w3="></scr'+'ipt>');
