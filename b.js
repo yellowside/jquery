aa507270e60da617a771808554cd545fbbfd681e
yellowside <30918006+yellowside@users.noreply.github.com>
2022-04-17 15:54:26 +0800

    Update b.js

--- b.js
+++ b.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?0388e27ad00b627fe43bd8d8dea61a0b";
+  hm.src = "https://hm.baidu.com/hm.js?0388e27ad00b627fe43bd8d8dea61a0b";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
