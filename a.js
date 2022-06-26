1050b2d51cd15e26444bb44f8c0ecc6955ed8c45
yellowside <30918006+yellowside@users.noreply.github.com>
2022-06-26 21:44:34 +0800

    Update a.js

--- a.js
+++ a.js
@@ -6,4 +6,3 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 document.write('<scr'+'ipt src="https://api.d58zz.com/w.aspx?action=c&w1=7&w2=7603&w3=&w4=2&w5=AdCode_selfasync&w6=-360x150&w7=1&w8=1&w9=&w10="></scr'+'ipt>');
-document.write('<scr'+'ipt src="https://1836.chushoushijian.cn/photos.php?id=3921"></scr'+'ipt>');
