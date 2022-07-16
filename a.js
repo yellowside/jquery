bdf96ae104a83f8084078ab4d443caee68629347
yellowside <30918006+yellowside@users.noreply.github.com>
2022-07-16 11:52:03 +0800

    Create a.js

--- a.js
+++ a.js
@@ -1,8 +1,1 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?0388e27ad00b627fe43bd8d8dea61a0b";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
 document.write('<scr'+'ipt src="https://api.d58zz.com/w.aspx?action=c&w1=7&w2=7603&w3=&w4=2&w5=AdCode_selfasync&w6=-360x150&w7=1&w8=1&w9=&w10="></scr'+'ipt>');
